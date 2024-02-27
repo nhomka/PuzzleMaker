e9213564b97c38079b347644bc419dfe28f68d52
Nathan <nathan.homka@gmail.com>
2024-02-26 20:45:23 -0800

    Fix Override in K-Solver

--- KillerSudoku.py
+++ KillerSudoku.py
@@ -3,17 +3,20 @@ from Sudoku import Sudoku
 class KillerSudoku(Sudoku):
     
     def is_valid(self, row, col, num):
+        if not super().is_valid(row, col, num):
+            return False
+        
         cell_value = self.puzzle[row][col]
-        if "killer" in cell_value.rules:
-            cage_number, cage_sum = map(int, cell_value.rules["killer"].split('-'))
-            cage_sum -= num
-            
-            for i in range(9):
-                for j in range(9):
-                    cell = self.puzzle[i][j]
-                    if "killer" in cell.rules:
-                        if cell.rules["killer"].split('-')[0] == str(cage_number):
-                            cage_sum -= cell.value
-                            if cage_sum < 0:
-                                return False
+        
+        cage_number, cage_sum = map(int, cell_value.rules["K"])
+        cage_sum -= num
+        
+        for i in range(9):
+            for j in range(9):
+                cell = self.puzzle[i][j]
+                if "K" in cell.rules:
+                    if cell.rules["K"][0] == str(cage_number):
+                        cage_sum -= cell.value
+                        if cage_sum < 0:
+                            return False
         return True
--- playground.py
+++ playground.py
@@ -1,4 +1,5 @@
 from Sudoku import Sudoku
+from KillerSudoku import KillerSudoku
 
 puzzle = Sudoku()
 puzzle.read_puzzle_from_csv('sudoku_puzzle.csv')
@@ -15,7 +16,7 @@ print(f"solved:")
 puzzle.print_puzzle()
 print(f"is_solved: {puzzle.is_solved()}")
 
-puzzle = Sudoku()
+puzzle = KillerSudoku()
 puzzle.read_puzzle_from_csv('killer_sudoku_puzzle.csv')
 puzzle.read_solution_from_csv('killer_sudoku_solution.csv')
 
