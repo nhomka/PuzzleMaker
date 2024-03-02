5c5a29a04c0b7a22199d676c32a61d4f65479cc4
Nathan <nathan.homka@gmail.com>
2024-03-02 13:10:30 -0800

    Initial react template

--- SudokuWebApp/sudoku-variants-app/src/App.js
+++ SudokuWebApp/sudoku-variants-app/src/App.js
@@ -1,4 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 function App() {
   const [data, setData] = useState({});
